839d2eba57645168e94623ac2728a9e749009205
suneelkumarpentela <suneeltherisingsun@gmail.com>
2021-03-15 20:58:33 +0530

    final commit

--- Assignment-1/stopwordRemoval.py
+++ Assignment-1/stopwordRemoval.py
@@ -1,10 +1,7 @@
 from util import *
 
-# Add your import statements here
 import nltk
 from nltk.corpus import stopwords
-
-
 
 class StopwordRemoval():
 
@@ -28,7 +25,6 @@ class StopwordRemoval():
 		stopwordRemovedText = []
 		stop_words = set(stopwords.words('english'))
 		
-		#Fill in code here
 		for i in range(len(text)):
 			word_tokens = text[i]
 			filtered_sentence = []
